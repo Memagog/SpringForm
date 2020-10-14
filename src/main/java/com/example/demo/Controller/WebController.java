29f7cb826571457448f58a424412b1144bbae693
Memagog <55204188+Memagog@users.noreply.github.com>
2020-10-14 12:57:22 +0300

    Created a controller and user class

---
+++ src/main/java/com/example/demo/Controller/WebController.java
@@ -0,0 +1,27 @@
+package com.example.demo.Controller;
+
+
+import com.example.demo.emplyonee.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class WebController {
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String userForm(Model model){
+        model.addAttribute("index", new User());
+            return "index";
+
+    }
+    @RequestMapping(value ="/index", method = RequestMethod.POST)
+    public String formSubmit(@ModelAttribute User user, Model model) {
+        model.addAttribute("index", user);
+        return "result";
+    }
+
+
+}
